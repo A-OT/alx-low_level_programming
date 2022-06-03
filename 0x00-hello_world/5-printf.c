45316878caa2afca167f8e0491281faa60d3290b
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-02 21:57:09 -0700

    g

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,12 @@
-/*
- * File: 5-printf.c
- * BY AOT
- */
-
 #include <stdio.h>
-
 /**
- * main - Prints "with proper grammar,but the outcome is
- * 		a piece of art," followed by a new line.
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
  *
- * Return: 0.
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
 	return (0);
 }
