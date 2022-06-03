9a092cfae320b2ea8723ba5c5b0c6a2ac2ce73d7
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-02 21:27:51 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Prints "Programming is like building a multilingual puzzle,
- * 				followed by a new line.
- *
- * Return: 0.
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-
 	return (0);
-
 }
