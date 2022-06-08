fdd415146ace7c783f6dbf55b7f5dce4a290c5fd
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-08 15:26:07 -0700

    Task 0 tried again

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include "main.h"
+
 /**
  * main - prints _putchar, followed by a new line
  *
  * Return: 0 (success)
  */
+
 int main(void)
+
 {
-	char word[8] = "_putchar";
+	char c[] = "_putchar";
 	int i;
-	char new_line = '\n';
 
 		for (i = 0; i < 8; i++)
 		{
-			write(1, &word[i], 1);
-				write(1, &new_line, 1);
+			_putchar(c[i]);
 		}
+
+		_putchar('\n');
 
 	return (0);
 }
