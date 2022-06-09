e54d2ce1c7fc6d86f3b6d6b9a6df5164c168d68f
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-09 04:45:58 -0700

    Task 5 tried

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if it's a letter, lowercase or uppercase.
+ *
+ * @c: the character to test.
+ *
+ * Return: 1 if true, 0 if not.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+			return (0);
+		else
+			return (1);
+}
