ad740067352d3cb04297948ae0554082bb6133de
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-09 06:56:20 -0700

    Task 9 tried again 2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,20 +10,16 @@
 void jack_bauer(void)
 {
 	int h, m;
-	int x1 = (h / 10);
-	int x2 = (h % 10);
-	int z1 = (m / 10);
-	int z2 = (m % 10);
 
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 64; m++)
 		{
-			_putchar(x1 + '0');
-			_putchar(x2 + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar(z1 + '0');
-			_putchar(z2 + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
 	}
