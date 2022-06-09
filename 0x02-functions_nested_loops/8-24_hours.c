bc0f266afb7c427d4ff534dda89009487e8ca77d
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-09 06:46:44 -0700

    Task 9 tried again

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,14 +4,27 @@
  * jack_bauer - function that prints every minute of the day
  * of Jack Bauer, starting from 00:00 to 23:59.
  *
- * @i: the character to be used.
- *
  * Return: 0 (success).
  */
 
-void jack_bauer(void i)
+void jack_bauer(void)
 {
-	if (i >= '00:00' && i <= '23:59')
-	       i += 1;
-		return (0);
+	int h, m;
+	int h1 = (h / 10);
+	int h2 = (h % 10);
+	int m1 = (m / 10);
+	int m2 = (m % 10);
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 64; m++)
+		{
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
+			_putchar(':');
+			_putchar(m1 + '0');
+			_putchar(m2 + '0');
+			_putchar('\n');
+		}
+	}
 }
