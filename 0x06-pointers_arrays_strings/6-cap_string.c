80a6e92ecc6455dbffcddd43eb8ab8b25cb82a2c
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-16 08:42:29 -0700

    Task 6 modified

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,31 +3,25 @@
 /**
  * cap_string - Function that capitalizes all words of a string.
  *
- * @z: input.
+ * @s: input.
  *
  * Return: capitalized words.
  */
 
-char *cap_string(char *z)
+char *cap_string(char *s)
 {
-	int a = 0, i;
-	int cps = 13;
-	char cp[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	while (z[a])
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
+	while (s[i] != '\0')
 	{
-		i = 0;
-
-		while (i < cps)
-		{
-			if ((a == 0 || z[a - 1] == cps[i]) && (z[a] >= 97 && z[a] <= 122))
-				z[a] -= 32;
-
-			i++;
-		}
-
-		a++;
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= ('a' - 'A');
+		i++;
 	}
-
 	return (z);
 }
