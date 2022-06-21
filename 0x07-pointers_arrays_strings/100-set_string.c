bf2882288c9569315c218f76b9a741ccd90cbff8
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-21 10:44:47 -0700

    Extra Task 1 done

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - Function that sets value of a pinter to a char.
+ *
+ * @s: input.
+ * @to: the char to point to.
+ *
+ * Return: 0 (success).
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
