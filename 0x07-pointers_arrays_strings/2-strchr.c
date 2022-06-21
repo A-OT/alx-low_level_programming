b37ab7af78d88e0fc045c5c90710b6dd67d91080
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-21 09:08:40 -0700

    Task 2 modified

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,20 +12,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i = 0;
 
-	while  (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-
-		else if (*(s + 1) == c)
-
-			return (s + 1);
-
-		s++;
-	}
-
-	return (s + 1);
+	for (i = 0; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
 
