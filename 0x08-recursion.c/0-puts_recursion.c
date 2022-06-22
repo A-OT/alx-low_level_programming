4cce415b300a42e9829a8de6580557bbcc35ba3e
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-22 05:50:43 -0700

    Task 0 tried

---
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _puts_recursion - Function that prints a string,
+ *  followed by a new line.
+ *
+ *  @s: Input string.
+ *
+ *  Return: 0 (success).
+ */
+
+void _puts_recursion(char *s)
+{
+	char s[] = "Lonely"
+
+		_putchar("I am %s here, I swear.\n", s)
+
+		return (0);
+}
