9cd579a11b49656c752e7c8ada27b85e6397eae4
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-22 06:01:17 -0700

    Task 0 modified

--- 0x08-recursion.c/0-puts_recursion.c
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -11,16 +11,9 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
+	char s[] = "Lonely";
 
-		return;
-	}
+	_putchar("I am %s here, i swear.\n", s)
 
-	_putchar(*s);
-
-	s++;
-
-	_puts_recursion(s);
+		return (0);
 }
