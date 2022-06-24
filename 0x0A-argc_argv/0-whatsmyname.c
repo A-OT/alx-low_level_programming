f5893066edec61e5085e96f5ba6722d568557abc
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-06-24 04:07:12 -0700

    Task 0 done, putchar file added, main head file added,  Readme file also added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints its name.
+ *
+ * @argc: number of argument.
+ * @argv: argument vector.
+ *
+ * Return: 0 (Success).
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
