c650f0faffe51d3534daf3a2d4276f826a014bfb
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-07-06 05:27:15 -0700

    Task 3-calc.h mof=dified

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -15,7 +15,7 @@ typedef struct op
 } op_t;
 
 int op_add(int a, int b);
-int op_ub(int a, int b);
+int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
