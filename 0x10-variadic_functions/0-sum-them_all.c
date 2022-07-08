627857cb2790d753fb42cf1577f05ce8dd354362
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-07-07 19:36:35 -0700

    Task 0 modified

--- 0x10-variadic_functions/0-sum-them_all.c
+++ 0x10-variadic_functions/0-sum-them_all.c
@@ -10,19 +10,16 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
-	int sum = 0;
+	unsigned int i, result;
+	va_list ap;
 
 	if (n == 0)
 		return (0);
-
-	va_start(valist, n);
-
-	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
-
-	va_end(valist);
-
-	return (sum);
+	va_start(ap, n);
+	for (i = result = 0; i < n; i++)
+	{
+		result += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (result);
 }
